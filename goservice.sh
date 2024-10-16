dfa835258f5f2552c03c1329742f3a6345d908df
julian <julian@codecollective.us>
2024-10-16 16:39:50 -0400

    simplifies diff for comparison with main fork

--- goservice.sh
+++
@@ -1,5 +0,0 @@
-# Use wait-for-it to wait until Keycloak is ready (on port 8888)
-./wait-for-it.sh localhost:8888 --timeout=60 --strict -- echo "Keycloak is up"
-
-# Once Keycloak is up, run the Go commands to initialize it (first go)
-go run ./service start
